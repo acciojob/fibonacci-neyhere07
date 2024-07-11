86ec0a45f042ee92266d1132399fd3586ac046ec
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-07-11 21:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const arr = [0,1];
 function fibonacci(num) {
 // your code here
+	if(num===0 || num===1)
+	  return arr[num];
+	  
+	 arr[num] = fibonacci(num-1)+fibonacci(num-2)
+	return arr[num];
 }
 
 module.exports = fibonacci;
